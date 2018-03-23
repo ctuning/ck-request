539c715b2c9cb9c23637ba939ac5df8296b93200
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-03-23 01:18:06 +0000

    fixes ...

--- module/request.asplos18/module.py
+++ module/request.asplos18/module.py
@@ -203,7 +203,7 @@ def show(i):
             ckey+'plot_variation_dimension2':[{'name':'no', 'value':'no'}, {'name':'yes', 'value':'yes'}]
           }
 
-    if i.get(ckey+'plot_dimension1','')=='': i[ckey+'plot_dimension1']=dimensions[2]['key']
+    if i.get(ckey+'plot_dimension1','')=='': i[ckey+'plot_dimension1']=dimensions[1]['key']
     if i.get(ckey+'plot_dimension2','')=='': i[ckey+'plot_dimension2']=dimensions[3]['key']
 
     if 'reset_'+form_name in i: reset=True
