4e1c8b137be8dab134cf9a2f8acce7b0c0343547
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-04-19 23:23:58 +0200

    clean up

--- module/request.asplos18/module.py
+++ module/request.asplos18/module.py
@@ -21,7 +21,7 @@ onchange='document.'+form_name+'.submit();'
 
 hextra='<center>\n'
 hextra+='<p>\n'
-hextra+=' Scoreboard prototype for <a href="http://cknowledge.org/request-cfp-asplos2018.html">ReQuEST@ASPLOS\'18 tournament</a>\n'
+hextra+=' Beta scoreboard for <a href="http://cknowledge.org/request-cfp-asplos2018.html">ReQuEST@ASPLOS\'18 AI/SW/HW co-design competition</a>\n'
 hextra+=' (your <a href="https://github.com/ctuning/ck-request/issues">feedback</a>)\n'
 hextra+='</center>\n'
 hextra+='<p>\n'
